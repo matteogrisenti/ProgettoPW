6951b6af6eba1a5725d65194a6fe604cc50ea08a
Matteo <116645666+matteogrisenti@users.noreply.github.com>
2023-05-05 20:27:03 +0200

    Aggiornamento Intestazione, Tag e Menu
    In questa commit ho aggiornato l'intestazione della pagina, ho definito il tag browser della pagina come chiesto nel documento e aggiornato il menu

---
+++ ProgettoPW/src/main/java/com/example/progettopw/Locandina.java
@@ -0,0 +1,16 @@
+package com.example.progettopw;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "Locandina", value = "/Locandina")
+public class Locandina extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+}
